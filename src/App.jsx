d2a5888a7ca15ac25d778dd4656d01345d7e70a2
bbuenodeveloper <b.buenoo94@gmail.com>
2025-04-29 01:59:50 -0300

    colocando os produtos na tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 function App() {
   const inputRef = useRef()
+  
+  const [produtos, setProdutos] = useState([])
 
 
   function cliqueiNoBotao(){
-    console.log(inputRef.current.value)
+      setProdutos([inputRef.current.value, ...produtos])
+      
   }
 
 
@@ -14,7 +17,13 @@ function App() {
       <h1 className="text-red-700 p-4 font-bold text-lg">Lista de Compras</h1>
       <input ref={inputRef}  placeholder="Produto..." className="outline-none  bg-gray-300 text-white px-2 py-2 m-4 " />
       <button onClick={cliqueiNoBotao} className="bg-gray-300 px-4 py-2 rounded-full hover:bg-blue-300 cursor-pointer">Adicionar</button>
+
+      {produtos.map((produto) => (
+        <div>{produto}</div>
+      ))}  
+
     </div>
+    
   )
 }
 
